a3702aafa11ba37117e51949ab02996f23358641
Ashish Virmani <virmani.ashish@gmail.com>
2015-01-07 23:44:44 -0800

    not using fixed port

--- menu_fetcher.js
+++ menu_fetcher.js
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
   }).end();
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address
   var port = server.address().port
